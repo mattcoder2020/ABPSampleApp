12baf781780e1ae7f42df531229fcc88a396b781
Matt Yang <matt.yang@commscope.com>
2023-08-04 20:14:34 +0800

    Finalized filtratio at backend

--- aspnet-core/src/AbpSampleApp.AccessManagment.Application/ApplicationServices/StoreApplicationService.cs
+++ aspnet-core/src/AbpSampleApp.AccessManagment.Application/ApplicationServices/StoreApplicationService.cs
@@ -1,13 +1,17 @@
 ﻿//using Abp.Application.Services.Dto;
+using Abp.Linq.Extensions;
 using AbpSampleApp.AccessManagment.AggregateRoots;
 using AbpSampleApp.AccessManagment.ApplicatinServices;
 using AbpSampleApp.AccessManagment.Dto;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Linq;
 
 namespace AbpSampleApp.AccessManagment.ApplicationServices
 {
@@ -25,30 +29,73 @@ namespace AbpSampleApp.AccessManagment.ApplicationServices
         {
             _storeRepository = repository;
         }
+        public async Task<PagedResultDto<StoreDto>> QueryByFiltration(StoreQueryDTO q)
+        {
+            var query = await _storeRepository.GetQueryableAsync();
+            query = query.Where(e => (q.Id == null || e.Id == q.Id)
+            && (q.Name.IsNullOrEmpty() || e.Name.Contains(q.Name))
+            && (q.CreationDate == null || e.CreationTime.Date == q.CreationDate));
 
-        //public async Task<List<Store>> QueryByFiltration(StoreQueryDTO q)
+            //if (q.Id.HasValue)
+            //{
+            //    query = query.Where(store => store.Id == q.Id.Value);
+            //}
+
+            //if (!string.IsNullOrEmpty(q.Name))
+            //{
+            //    query = query.Where(store => store.Name.Contains(q.Name));
+            //}
+
+            //if (q.CreationDate.HasValue)
+            //{
+            //    query = query.Where(store => store.CreationTime == q.CreationDate.Value);
+            //}
+
+            var totalCount = await query.CountAsync();
+
+            //var filteredQuery = ApplySorting(query, q);
+
+            var items = await query.ToListAsync();
+
+            var dtos = ObjectMapper.Map<List<Store>, List<StoreDto>>(items);
+
+            return new PagedResultDto<StoreDto>(totalCount, dtos);
+            
+        }
+        //public async Task<PagedResultDto<Store>> QueryByFiltration(StoreQueryDTO q)
         //{
-        //    //return await _storeRepository.GetListAsync(e => 
-        //    //    (q.Id == null  || e.Id == q.Id)
-        //    // && (q.Name.IsNullOrEmpty() || e.Name.Contains(q.Name))
-        //    //  && (q.CreationDate== null || e.CreationTime.Date == q.CreationDate), false);
+        //    //how to query and return pageinated entities 
 
+        //    return await base.GetListAsync()
+        //    //return await _storeRepository.GetListAsync(e =>
+        //    //    (q.Id == null || e.Id == q.Id)
+        //    // && (q.Name.IsNullOrEmpty() || e.Name.Contains(q.Name))
+        //    //  && (q.CreationDate == null || e.CreationTime.Date == q.CreationDate), false);
+        //    base.del
+        //    var pagedRequestDto = new PagedAndSortedResultRequestDto();
+        //    pagedRequestDto = pagedRequestDto..WhereIf<StoreQueryDTO, bool>(true, (e =>
+        //        (q.Id == null || e.Id == q.Id)
+        //     && (q.Name.IsNullOrEmpty() || e.Name.Contains(q.Name))
+        //      && (q.CreationDate == null || e.CreationTime.Date == q.CreationDate)));
+
+
+        //    base.GetListAsync()
         //    return await _storeRepository..GetPagedListAsync(e => e.Name.Contains(q.Name));
-          
+
         //}
 
-        public async  Task<PagedResultDto<StoreDto>> QueryByFiltration(StoreQueryDTO q)
-        {
-            PagedAndSortedResultRequestDto input;
-            var queryable = await base.CreateFilteredQueryAsync(input);
+        //public async  Task<PagedResultDto<StoreDto>> QueryByFiltration(StoreQueryDTO q)
+        //{
+        //    PagedAndSortedResultRequestDto input;
+        //    var queryable = await base.CreateFilteredQueryAsync(input);
 
-            // You can add additional filtering, sorting, or any other operations here if needed.
+        //    // You can add additional filtering, sorting, or any other operations here if needed.
 
-            var totalCount = await AsyncExecuter.CountAsync(queryable);
-            var items = await AsyncExecuter.ToListAsync(queryable.PageBy(input));
+        //    var totalCount = await AsyncExecuter.CountAsync(queryable);
+        //    var items = await AsyncExecuter.ToListAsync(queryable.PageBy(input));
 
-            return new PagedResultDto<StoreDto>(totalCount, ObjectMapper.Map<List<Store>, List<StoreDto>>(items));
-        }
+        //    return new PagedResultDto<StoreDto>(totalCount, ObjectMapper.Map<List<Store>, List<StoreDto>>(items));
+        //}
 
 
 
