bef90106d6e81f891829b8a684a025af1723be67
mattcoder2017 <myang@arris.com>
2023-08-09 15:27:55 +0800

    Done for the microsoft and mac platform

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,23 +3,23 @@ import { Environment } from '@abp/ng.core';
 const baseUrl = 'http://localhost:4200';
 
 export const environment = {
-  production: true,
+  production: false,
   application: {
     baseUrl,
     name: 'AccessManagment',
     logoUrl: '',
   },
-  oAuthConfig: {
-    issuer: 'https://localhost:44357/',
+  oAuthConfig: {    
+    issuer: 'backend',
     redirectUri: baseUrl,
     clientId: 'AccessManagment_App',
     responseType: 'code',
     scope: 'offline_access AccessManagment',
-    requireHttps: true
+    requireHttps: true,
   },
   apis: {
-    default: {
-      url: 'https://localhost:44357',
+    default: {     
+      url: 'backend',
       rootNamespace: 'AbpSampleApp.AccessManagment',
     },
   },
